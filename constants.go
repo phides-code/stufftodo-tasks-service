8afc5726164fab5667df936c76767e03fe5ceecf
Philippe DeSousa <phil.desousa@gmail.com>
2025-02-20 17:54:31 -0500

    update AllowOrigin

--- constants.go
+++ constants.go
@@ -1,7 +1,8 @@
 package main
 
 // replace with frontend app URL
-const OriginURL = "http://localhost:5173"
+// const OriginURL = "http://localhost:5173"
+const OriginURL = "https://d3gvjncxxemfbd.cloudfront.net/"
 
 // replace with table name
 const TableName = "StufftodoTasks"
